74e8ed7917fff52a2d50ec2888413c6eff54282d
Shuguang_Liux <Shuguang_Liux@outlook.com>
2020-09-29 00:13:37 +0800

    增加webservice

---
+++ tcgl-service/src/main/java/com/record/tcgl/webservice/GetUserInfo.java
@@ -0,0 +1,60 @@
+
+package com.record.tcgl.webservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * &lt;p&gt;getUserInfo complex type�� Java �ࡣ
+ * 
+ * &lt;p&gt;����ģʽƬ��ָ�������ڴ����е�Ԥ�����ݡ�
+ * 
+ * &lt;pre&gt;
+ * &amp;lt;complexType name="getUserInfo"&amp;gt;
+ *   &amp;lt;complexContent&amp;gt;
+ *     &amp;lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&amp;gt;
+ *       &amp;lt;sequence&amp;gt;
+ *         &amp;lt;element name="userName" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&amp;gt;
+ *       &amp;lt;/sequence&amp;gt;
+ *     &amp;lt;/restriction&amp;gt;
+ *   &amp;lt;/complexContent&amp;gt;
+ * &amp;lt;/complexType&amp;gt;
+ * &lt;/pre&gt;
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getUserInfo", propOrder = {
+    "userName"
+})
+public class GetUserInfo {
+
+    protected String userName;
+
+    /**
+     * ��ȡuserName���Ե�ֵ��
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * ����userName���Ե�ֵ��
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setUserName(String value) {
+        this.userName = value;
+    }
+
+}
---
+++ tcgl-service/src/main/java/com/record/tcgl/webservice/GetUserInfoResponse.java
@@ -0,0 +1,62 @@
+
+package com.record.tcgl.webservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * &lt;p&gt;getUserInfoResponse complex type�� Java �ࡣ
+ * 
+ * &lt;p&gt;����ģʽƬ��ָ�������ڴ����е�Ԥ�����ݡ�
+ * 
+ * &lt;pre&gt;
+ * &amp;lt;complexType name="getUserInfoResponse"&amp;gt;
+ *   &amp;lt;complexContent&amp;gt;
+ *     &amp;lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&amp;gt;
+ *       &amp;lt;sequence&amp;gt;
+ *         &amp;lt;element name="return" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&amp;gt;
+ *       &amp;lt;/sequence&amp;gt;
+ *     &amp;lt;/restriction&amp;gt;
+ *   &amp;lt;/complexContent&amp;gt;
+ * &amp;lt;/complexType&amp;gt;
+ * &lt;/pre&gt;
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getUserInfoResponse", propOrder = {
+    "_return"
+})
+public class GetUserInfoResponse {
+
+    @XmlElement(name = "return")
+    protected String _return;
+
+    /**
+     * ��ȡreturn���Ե�ֵ��
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getReturn() {
+        return _return;
+    }
+
+    /**
+     * ����return���Ե�ֵ��
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setReturn(String value) {
+        this._return = value;
+    }
+
+}
