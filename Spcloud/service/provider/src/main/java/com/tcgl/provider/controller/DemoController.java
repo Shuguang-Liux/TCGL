079856b7dd579f579b0597d303a6f58887f06409
Shuguang_Liux <Shuguang_Liux@outlook.com>
2021-11-03 16:56:06 +0800

    可执行通的初始版本

--- Spcloud/service/provider/src/main/java/com/tcgl/provider/controller/DemoController.java
+++ Spcloud/service/provider/src/main/java/com/tcgl/provider/controller/DemoController.java
@@ -1,7 +1,9 @@
 package com.tcgl.provider.controller;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/10/29 18:25
  */
 @RestController
-public class DemoContoller {
+//@RequestMapping("/provider")
+@RefreshScope
+public class DemoController {
     @GetMapping(value = "/demo/{string}")
     public String demoTesting(@PathVariable("string") String string){
         return "hello" + string;
