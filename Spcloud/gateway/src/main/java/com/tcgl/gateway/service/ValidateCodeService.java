2b7873024f7c67aad87b69692c6720205eaf8cf8
Shuguang_Liux <Shuguang_Liux@outlook.com>
2022-04-08 17:27:44 +0800

    remove dingdingInfo add gateway

---
+++ Spcloud/gateway/src/main/java/com/tcgl/gateway/service/ValidateCodeService.java
@@ -0,0 +1,24 @@
+package com.tcgl.gateway.service;
+
+import com.tcgl.common.core.exception.CaptchaException;
+import com.tcgl.common.core.web.domain.AjaxResult;
+
+import java.io.IOException;
+
+/**
+ * 验证码处理
+ *
+ * @author ruoyi
+ */
+public interface ValidateCodeService
+{
+    /**
+     * 生成验证码
+     */
+    public AjaxResult createCaptcha() throws IOException, CaptchaException;
+
+    /**
+     * 校验验证码
+     */
+    public void checkCaptcha(String key, String value) throws CaptchaException;
+}
